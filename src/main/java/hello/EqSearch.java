5fc7d38fddb5b69a4d70db2befa09af234b22587
chen huang <chenhuang@chens-MacBook-Pro.local>
2019-11-19 00:10:47 -0800

    ch finish step8

---
+++ src/main/java/hello/EqSearch.java
@@ -0,0 +1,23 @@
+package hello;
+
+public class EqSearch { 
+	private int distance;
+ 	private int minmag;
+ 	public EqSearch(){}
+	public void setDistance(int d) 
+		{ 
+			distance = d; 
+		} 
+	public void setMinmag(int m) 
+		{ 
+			minmag = m; 
+		}
+	public int getDistance() 
+		{ 
+			return distance; 
+		} 
+	public int getMinmag() 
+		{ 
+			return minmag; 
+		}
+} 
